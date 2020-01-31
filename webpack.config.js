7da3ad288097a7c854f2c20dc68bd909af3c9cf3
Ruben Costa <costamartinezruben@gmail.com>
2020-01-31 23:03:22 +0000

    comment bundleAnalyzerPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const babelConfig = require('./babel.config')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+//   .BundleAnalyzerPlugin
 
 module.exports = {
   resolve: {
     extensions: ['.js'],
-    // mainFields: ['main', 'module'], // https://github.com/webpack/webpack/issues/5756#issuecomment-405468106
+    mainFields: ['main', 'module'], // https://github.com/webpack/webpack/issues/5756#issuecomment-405468106
   },
   devtool: 'source-map',
   mode: 'development',
-  plugins: [new BundleAnalyzerPlugin()],
+  // plugins: [new BundleAnalyzerPlugin()],
   module: {
     rules: [
       {
