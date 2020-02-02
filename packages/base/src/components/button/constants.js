ae2c317dd8e0562ce8a77df130428e029dc74383
Ruben Costa <costamartinezruben@gmail.com>
2020-02-02 20:01:06 +0000

    add button constants

---
+++ packages/base/src/components/button/constants.js
@@ -0,0 +1,20 @@
+export const TYPE = {
+  primary: 'primary',
+  secondary: 'secondary',
+  tertiary: 'tertiary',
+  minimal: 'minimal',
+}
+
+export const SHAPE = {
+  default: 'default',
+  pill: 'pill',
+  round: 'round',
+  square: 'square',
+}
+
+export const SIZE = {
+  mini: 'mini',
+  default: 'default',
+  compact: 'compact',
+  large: 'large',
+}
