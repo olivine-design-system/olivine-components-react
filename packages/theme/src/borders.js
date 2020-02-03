03bf2f05704844898903105a23c7f5d21be37b71
Ruben Costa <costamartinezruben@gmail.com>
2020-02-03 00:41:04 +0000

    add borders

---
+++ packages/theme/src/borders.js
@@ -0,0 +1,45 @@
+// default borderColor -> light theme.
+// Dark theme overrides the border colors.
+const borders = {
+  border100: {
+    borderColor: 'hsla(0, 0%, 0%, 0.04)',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+  },
+  border200: {
+    borderColor: 'hsla(0, 0%, 0%, 0.08)',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+  },
+  border300: {
+    borderColor: 'hsla(0, 0%, 0%, 0.12)',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+  },
+  border400: {
+    borderColor: 'hsla(0, 0%, 0%, 0.16)',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+  },
+  border500: {
+    borderColor: 'hsla(0, 0%, 0%, 0.2)',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+  },
+  border600: {
+    borderColor: 'hsla(0, 0%, 0%, 0.24)',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+  },
+  radius100: '2px',
+  radius200: '4px',
+  radius300: '8px',
+  radius400: '12px',
+  useRoundedCorners: true,
+  buttonBorderRadius: '0px',
+  inputBorderRadius: '0px',
+  popoverBorderRadius: '0px',
+  surfaceBorderRadius: '0px',
+}
+
+export default borders
