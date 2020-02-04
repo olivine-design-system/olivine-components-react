d914c50ac470e05b129a94860aa626c2235173ab
Ruben Costa <costamartinezruben@gmail.com>
2020-02-04 23:45:12 +0000

    map colors

---
+++ packages/theme/src/themes/light-theme/components-colors.js
@@ -0,0 +1,45 @@
+import colors from './color-tokens.js'
+
+export default {
+  // Buttons
+  buttonPrimaryFill: colors.primary,
+  buttonPrimaryText: colors.white,
+  buttonPrimaryHover: colors.primary700,
+  buttonPrimaryActive: colors.primary600,
+  buttonPrimarySelectedFill: colors.primary600,
+  buttonPrimarySelectedText: colors.white,
+  buttonPrimarySpinnerForeground: colors.primary50,
+  buttonPrimarySpinnerBackground: colors.primary500,
+
+  buttonSecondaryFill: colors.primary100,
+  buttonSecondaryText: colors.primary,
+  buttonSecondaryHover: colors.primary200,
+  buttonSecondaryActive: colors.primary300,
+  buttonSecondarySelectedFill: colors.primary200,
+  buttonSecondarySelectedText: colors.primary,
+  buttonSecondarySpinnerForeground: colors.primary700,
+  buttonSecondarySpinnerBackground: colors.primary300,
+
+  buttonTertiaryFill: 'transparent',
+  buttonTertiaryText: colors.primary,
+  buttonTertiaryHover: colors.primary50,
+  buttonTertiaryActive: colors.primary100,
+  buttonTertiarySelectedFill: colors.primary100,
+  buttonTertiarySelectedText: colors.primary,
+  buttonTertiarySpinnerForeground: colors.primary700,
+  buttonTertiarySpinnerBackground: colors.primary300,
+
+  buttonMinimalFill: 'transparent',
+  buttonMinimalText: colors.primary,
+  buttonMinimalHover: colors.primary50,
+  buttonMinimalActive: colors.primary100,
+  buttonMinimalSelectedFill: colors.primary100,
+  buttonMinimalSelectedText: colors.primary,
+  buttonMinimalSpinnerForeground: colors.primary700,
+  buttonMinimalSpinnerBackground: colors.primary300,
+
+  buttonDisabledFill: colors.mono200,
+  buttonDisabledText: colors.mono600,
+  buttonDisabledSpinnerForeground: colors.mono600,
+  buttonDisabledSpinnerBackground: colors.mono400,
+}
