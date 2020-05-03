d41f7685eb462d61f53c4c5cd58ed9d8054c3922
Ruben Costa <costamartinezruben@gmail.com>
2020-05-03 01:13:02 +0100

    fix card throwing an error when visualized in storybook - theme couldn't be found

--- packages/card/src/index.stories.js
+++ packages/card/src/index.stories.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { withKnobs, color, text } from '@storybook/addon-knobs'
+import { withKnobs, text } from '@storybook/addon-knobs'
 import { withReadme } from 'storybook-readme'
-import { ThemeProvider } from 'emotion-theming'
+import { testingUtilities } from '@olivine/internals'
 
-import { createTheme } from '../../theme/src'
 import CardReadme from '../README.md'
 
 import Card from '.'
+
+const { ThemeProviderMock } = testingUtilities
 
 // the only reason onClick was written like this, and not just
 // `onClick = action('clicked')` is due to a cleaner export in the JSX tab
@@ -28,19 +29,14 @@ storiesOf('Card', module)
     )
     const buttonText = text('Button Text', 'Click me', 'Data')
 
-    // styling and theme
-    const accent = color('Accent Color', '#9FB1BC', 'Colors')
-    const bodyColor = color('Body Color', '#465775', 'Colors')
-    const theme = createTheme({ colors: { bodyColor, accent } })
-
     return (
-      <ThemeProvider theme={theme}>
+      <ThemeProviderMock>
         <div style={{ margin: '0 auto', width: '300px' }}>
           {/* <GlobalStyle /> */}
           <Card buttonText={buttonText} onClick={onClick} title={cardTitle}>
             {cardText}
           </Card>
         </div>
-      </ThemeProvider>
+      </ThemeProviderMock>
     )
   })
